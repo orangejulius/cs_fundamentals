ee990777e7d9398ccae1c8ebc188f21d10c52149
Julian Simioni <julian.simioni@gmail.com>
2012-05-17 13:39:21 -0700

    Add functions to insert into and print singly linked list

--- linked_list.c
+++ linked_list.c
@@ -9,4 +9,13 @@ int main()
 	//initialize the first entry in the list
 	singly_linked_list *head = init_singly_linked_list(&data[0]);
 	printf("initialized singly_linked_list at %p with data %d\n", head, *(int *)(head->data));
+
+	//insert the rest of the data into the list
+	int i;
+	for (i = 1; i < 10; i++) {
+		singly_linked_list *new_item = init_singly_linked_list(&data[i]);
+		insert_singly_linked_list(head, new_item);
+	}
+
+	print_singly_linked_list(head);
 }
--- linked_list.h
+++ linked_list.h
@@ -23,4 +23,32 @@ singly_linked_list *init_singly_linked_list(void *data)
 
 	return list;
 }
+
+void insert_singly_linked_list(singly_linked_list *head, singly_linked_list *new_item)
+{
+	singly_linked_list *list = head;
+
+	//traverse the length of the list
+	while (list->next != 0) {
+		list = list->next;
+	}
+
+	//insert the new item into the list
+	list->next = new_item;
+	new_item->next = 0; //just in case
+}
+
+void print_singly_linked_list(singly_linked_list *head) {
+	if (head == 0) {
+		return;
+	}
+
+	singly_linked_list *list = head;
+	int i = 0;
+
+	do {
+		printf("%d: address: %p data: %d\n", i, list, *(int *)(list->data));
+		i++;
+	} while (list = list->next); // loop continues if list is not 0
+}
 #endif // LINKED_LIST_H
