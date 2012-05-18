6d748cf9072c07247c874f5a762484b3323e7895
Julian Simioni <julian.simioni@gmail.com>
2012-05-17 21:44:45 -0700

    Add a new header with basic binary tree operations
    Currently supports struct initialization, recursive insertion, recursive
    inorder printing

---
+++ binary_tree.h
@@ -0,0 +1,70 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+#include <stdlib.h>
+
+struct tree_node;
+
+/*
+ * Tree node definition
+ * To focus on algorithms and not handling comparisons of abitrary data types,
+ * each node just contains an integer as data
+ */
+typedef struct tree_node {
+	int data;
+	struct tree_node *left;
+	struct tree_node *right;
+} tree_node;
+
+/*
+ * Given some data return a newly allocated and initialized tree_node
+ */
+tree_node *init_tree_node(int data)
+{
+	tree_node *new = malloc(sizeof(tree_node));
+	new->data = data;
+	new->left = 0;
+	new->right = 0;
+
+	return new;
+}
+
+/*
+ * Given a root node and a new node, insert the new node into the correct
+ * place in the tree pointed to by root node
+ * Uses recursive function calls
+ * O(log(N)) average complexity
+ */
+void insert_tree_node_recursive(tree_node *root, tree_node *new)
+{
+	if (new->data > root->data) {
+		if (root->right) {
+			insert_tree_node_recursive(root->right, new);
+		} else {
+			root->right = new;
+		}
+	} else {
+		if (root->left) {
+			insert_tree_node_recursive(root->left, new);
+		} else {
+			root->left = new;
+		}
+	}
+}
+
+/*
+ * Given a root node, print all items in the tree using an inorder traversal.
+ * Uses recursive function calls
+ * All items are printed on a single line with no newline at the end
+ */
+void print_tree_inorder_recursive(tree_node *root)
+{
+	if (!root) {
+		return;
+	}
+	print_tree_inorder_recursive(root->left);
+	printf("%d, ", root->data);
+	print_tree_inorder_recursive(root->right);
+}
+
+#endif // BINARY_TREE_H
