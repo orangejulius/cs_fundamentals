c12fc99883b62ded3281d85fdf96da9a9edbfcee
Julian Simioni <julian.simioni@gmail.com>
2012-05-21 20:16:23 -0700

    Use the new singly_linked_list struct

--- linked_list.c
+++ linked_list.c
@@ -6,24 +6,21 @@ int main()
 {
 	int data[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	//initialize the first entry in the list
-	singly_linked_list_node *head = init_singly_linked_list_node(&data[0]);
-	printf("initialized singly_linked_list_node at %p with data %d\n", head, *(int *)(head->data));
+	//initialize the linked list struct
+	singly_linked_list *list = init_singly_linked_list();
 
-	//insert the rest of the data into the list
+	//insert the data into the list
 	int i;
-	singly_linked_list_node *entry = head; //keep track of the last entry so insert_after can be used
-	for (i = 1; i < 10; i++) {
+	for (i = 0; i < 10; i++) {
 		singly_linked_list_node *new_item = init_singly_linked_list_node(&data[i]);
-		insert_after_singly_linked_list_node(entry, new_item);
-		entry = new_item;
+		insert_tail_singly_linked_list(list, new_item);
 	}
 
-	print_singly_linked_list_node(head);
+	print_singly_linked_list_node(list->head);
 
-	head = reverse_singly_linked_list(head);
+	list->head = reverse_singly_linked_list(list->head);
 
 	printf("\nReversing the list!\n");
 
-	print_singly_linked_list_node(head);
+	print_singly_linked_list_node(list->head);
 }
