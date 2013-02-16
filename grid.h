e4376808b1ad07e7af1ed3eb5f9207ea72a2b9b1
Julian Simioni <julian.simioni@gmail.com>
2013-02-15 17:55:36 -0800

    Create second grid sum function that iterates over columns before rows
    This should be significantly slower due to cache locality.

--- grid.h
+++ grid.h
@@ -11,3 +11,15 @@ int grid_sum(int *grid, int x, int y, int x1, int y1, int x2, int y2)
 	}
 	return sum;
 }
+
+int grid_sum_y(int *grid, int x, int y, int x1, int y1, int x2, int y2)
+{
+	int sum = 0;
+	int i, j;
+	for (i = x1; i <= x2; i++) {
+		for (j = y1; j <= y2; j++) {
+			sum += grid[i*y + j];
+		}
+	}
+	return sum;
+}
