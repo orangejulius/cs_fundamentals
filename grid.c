7001590e7185c9839c517f6379df583196147c0c
Julian Simioni <julian.simioni@gmail.com>
2013-02-15 18:49:09 -0800

    Display time difference for summing 10000x10000 grid by row or column first

--- grid.c
+++ grid.c
@@ -1,10 +1,21 @@
 #include <assert.h>
 #include <stdio.h>
+#include <sys/time.h>
 
 #include "grid.h"
 
 #define GRID_X 10
 #define GRID_Y 10
+
+#define GRID2_X 10000
+#define GRID2_Y 10000
+
+double microTime()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec*1000000.0 + tv.tv_usec;
+}
 
 int main()
 {
@@ -21,4 +32,25 @@ int main()
 
 	assert(grid_sum((int*)&grid, GRID_X, GRID_Y, 1, 1, 2, 2) == 9);
 	assert(grid_sum_y((int*)&grid, GRID_X, GRID_Y, 1, 1, 2, 2) == 9);
+
+	int* grid2 = malloc(sizeof(int)*GRID2_X*GRID2_Y);
+
+	for (i = 0; i < GRID2_X; i++) {
+		for(j = 0; j < GRID2_Y; j++) {
+			grid2[j*GRID2_X + i] = 0;
+		}
+	}
+
+	double start = microTime();
+
+	int sum1 = grid_sum(grid2, GRID2_X, GRID2_Y, 0, 0, GRID2_X-1, GRID2_Y-1);
+
+	double mid = microTime();
+
+	int sum2 = grid_sum_y(grid2, GRID2_X, GRID2_Y, 0, 0, GRID2_X-1, GRID2_Y-1);
+
+	double end = microTime();
+
+	printf("%f\n", mid - start);
+	printf("%f\n", end - mid);
 }
