d63483b37c63cd02ae709f65434c85a43b511941
Julian Simioni <julian.simioni@gmail.com>
2013-02-16 22:42:15 -0800

    Swap the functionality of the two sum fuctions
    The first was actually sorting column first and thus had significantly
    worse performance due to cache usage.

--- grid.h
+++ grid.h
@@ -6,7 +6,7 @@ int grid_sum(int *grid, int x, int y, int x1, int y1, int x2, int y2)
 	int i, j;
 	for (i = x1; i <= x2; i++) {
 		for (j = y1; j <= y2; j++) {
-			sum += grid[j*x + i];
+			sum += grid[i*x + j];
 		}
 	}
 	return sum;
@@ -18,7 +18,7 @@ int grid_sum_y(int *grid, int x, int y, int x1, int y1, int x2, int y2)
 	int i, j;
 	for (i = x1; i <= x2; i++) {
 		for (j = y1; j <= y2; j++) {
-			sum += grid[i*y + j];
+			sum += grid[j*y + i];
 		}
 	}
 	return sum;
