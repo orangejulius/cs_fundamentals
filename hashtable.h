deb73e1fde67bf0b7a0031f83bb0206d01ad1cf2
Julian Simioni <julian.simioni@gmail.com>
2013-02-27 18:17:38 -0800

    Support updating values already in hashtable

--- hashtable.h
+++ hashtable.h
@@ -44,28 +44,41 @@ hashtable *hashtable_init(int num_buckets, int (*hash_fn)(void*))
 	return ht;
 }
 
-void hashtable_insert(hashtable *ht, char *key, void *data)
-{
-	int bucket = ht->hash_fn(key) % ht->num_buckets;
-
-	hashtable_item *item = init_hashtable_item(key, data);
-
-	insert_singly_linked_list(ht->buckets[bucket], item);
-
-}
-
-void* hashtable_find(hashtable* ht, char *key)
+// internal helper functgion to find hashtable_item with given key
+hashtable_item* hashtable_find_item(hashtable* ht, char *key)
 {
 	int bucket = ht->hash_fn(key) % ht->num_buckets;
 
 	singly_linked_list_node *node = ht->buckets[bucket]->head;
+	if (!node) return 0;
 
 	do {
 		hashtable_item *item = node->data;
 		if (item->key == key)
-			return item->data;
+			return item;
 	} while (node = node->next);
 
 	return 0;
+}
+
+//will update value if it already exists
+void hashtable_insert(hashtable *ht, char *key, void *data)
+{
+	int bucket = ht->hash_fn(key) % ht->num_buckets;
+
+	hashtable_item *item;
+	item = hashtable_find_item(ht, key);
+	if (item) {
+		item->data = data;
+	} else {
+		item = init_hashtable_item(key, data);
+		insert_singly_linked_list(ht->buckets[bucket], item);
+	}
+}
+
+void* hashtable_find(hashtable* ht, char *key)
+{
+	hashtable_item *item = hashtable_find_item(ht, key);
+	return item ? item->data : 0;
 }
 #endif // HASHTABLE_H
