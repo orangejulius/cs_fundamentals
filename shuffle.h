c420ba0d95468ef1096fc243d1b232e86f902a7b
Julian Simioni <julian.simioni@gmail.com>
2013-03-06 21:51:22 -0800

    Fix out of bounds access in shuffle

--- shuffle.h
+++ shuffle.h
@@ -17,7 +17,7 @@ char* array_to_string(int array[], int size)
 
 void shuffle(int array[], int size)
 {
-	for (int i = size; i > 0; i--) {
+	for (int i = size - 1; i > 0; i--) {
 		int j = rand() % i;
 		int temp = array[i];
 		array[i] = array[j];
