33e534e90350f86d2302990b0032738b1ecf4a11
Julian Simioni <julian.simioni@gmail.com>
2013-03-06 21:49:54 -0800

    Abstract sort testing to single function
    This allows many sorting methods to be tested easily

--- sort.c
+++ sort.c
@@ -1,14 +1,24 @@
 #include <assert.h>
 
+#include "shuffle.h"
 #include "sort.h"
+
+void test_sort(void (*sort_fn)(int[], int), int size)
+{
+	int *data = malloc(sizeof(int) * size);
+	for (int i = 0; i < size; i++) {
+		data[i] = i;
+	}
+
+	shuffle(data, size);
+	sort_fn(data, size);
+
+	for(int i = 0; i < size; i++) {
+		assert(i == data[i]);
+	}
+}
 
 int main()
 {
-	int data[] = {3, 5, 7, 2, 1, 9, 0, 8, 6, 4};
-
-	bubble_sort(data, 10);
-
-	for(int i = 0; i < 10; i++) {
-		assert(i == data[i]);
-	}
+	test_sort(bubble_sort, 10);
 }
