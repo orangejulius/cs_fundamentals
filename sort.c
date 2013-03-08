330b4a18acf19732f5909fe681d291c7a0368e8d
Julian Simioni <julian.simioni@gmail.com>
2013-03-07 22:43:25 -0800

    Add benchmark_sort function

--- sort.c
+++ sort.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <sys/time.h>
 
 #include "shuffle.h"
 #include "sort.h"
@@ -18,8 +19,27 @@ void test_sort(void (*sort_fn)(int[], int), int size)
 	}
 }
 
+void benchmark_sort(void (*sort_fn)(int[], int), int size, int iterations)
+{
+	struct timeval start;
+	gettimeofday(&start, 0);
+
+	for (int i = 0; i < iterations; i++) {
+		test_sort(sort_fn, size);
+	}
+
+	struct timeval end;
+	gettimeofday(&end, 0);
+
+	double elapsed_time = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec)/1e6;
+	printf("ran %d iterations in %f seconds\n", iterations, elapsed_time);
+}
+
 int main()
 {
 	test_sort(bubble_sort, 10);
 	test_sort(naive_bubble_sort, 10);
+
+	benchmark_sort(bubble_sort, 100, 100000);
+	benchmark_sort(naive_bubble_sort, 100, 100000);
 }
