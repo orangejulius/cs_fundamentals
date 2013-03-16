bf8d9f52abb688a8a4ff8a30e7a266f67e0ff8da
Julian Simioni <julian.simioni@gmail.com>
2013-03-15 22:14:16 -0700

    Run benchmarks for each sort on various size lists

--- sort.c
+++ sort.c
@@ -19,7 +19,7 @@ void test_sort(void (*sort_fn)(int[], int), int size)
 	}
 }
 
-void benchmark_sort(void (*sort_fn)(int[], int), int size, int iterations)
+double benchmark_sort(void (*sort_fn)(int[], int), int size, int iterations)
 {
 	struct timeval start;
 	gettimeofday(&start, 0);
@@ -31,8 +31,7 @@ void benchmark_sort(void (*sort_fn)(int[], int), int size, int iterations)
 	struct timeval end;
 	gettimeofday(&end, 0);
 
-	double elapsed_time = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec)/1e6;
-	printf("ran %d iterations in %f seconds\n", iterations, elapsed_time);
+	return end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec)/1e6;
 }
 
 int main()
@@ -41,7 +40,16 @@ int main()
 	test_sort(naive_bubble_sort, 10);
 	test_sort(bogo_sort, 10);
 
-	benchmark_sort(bubble_sort, 100, 100000);
-	benchmark_sort(naive_bubble_sort, 100, 100000);
-	benchmark_sort(insertion_sort, 100, 100000);
+	double time;
+	for (int i = 0; i < 10; i++) {
+		time = benchmark_sort(bubble_sort, i, 100000);
+		printf("bubble sorted %d entries in %f seconds\n", i, time);
+		time = benchmark_sort(naive_bubble_sort, i, 100000);
+		printf("naievely bubble sorted %d entries in %f seconds\n", i, time);
+		time = benchmark_sort(insertion_sort, i, 100000);
+		printf("insertion sorted %d entries in %f seconds\n", i, time);
+		time = benchmark_sort(bogo_sort, i, 100000);
+		printf("bogo sorted %d entries in %f seconds\n", i, time);
+		printf("\n");
+	}
 }
