3c6d3df2445dca8c4c3a6554f86b03094651a4c1
Julian Simioni <julian.simioni@gmail.com>
2013-04-03 15:06:36 -0700

    Correctly calculate random index in shuffle

--- shuffle.h
+++ shuffle.h
@@ -18,7 +18,7 @@ char* array_to_string(int array[], int size)
 void shuffle(int array[], int size)
 {
 	for (int i = size - 1; i > 0; i--) {
-		int j = rand() % i;
+		int j = rand() % (i + 1);
 		int temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
