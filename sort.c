35442e4a369eeb077450d9922d15d70b8eb5bcfe
Julian Simioni <julian.simioni@gmail.com>
2013-04-05 09:38:36 -0700

    Print more readable benchmark output

--- sort.c
+++ sort.c
@@ -34,22 +34,36 @@ double benchmark_sort(void (*sort_fn)(int[], int), int size, int iterations)
 	return end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec)/1e6;
 }
 
+typedef struct sort_fn_info
+{
+	char* name;
+	void (*sort_fn)(int[], int);
+} sort_fn_info;
+
 int main()
 {
 	test_sort(bubble_sort, 10);
 	test_sort(naive_bubble_sort, 10);
 	test_sort(bogo_sort, 10);
 
-	double time;
-	for (int i = 0; i < 10; i++) {
-		time = benchmark_sort(bubble_sort, i, 100000);
-		printf("bubble sorted %d entries in %f seconds\n", i, time);
-		time = benchmark_sort(naive_bubble_sort, i, 100000);
-		printf("naievely bubble sorted %d entries in %f seconds\n", i, time);
-		time = benchmark_sort(insertion_sort, i, 100000);
-		printf("insertion sorted %d entries in %f seconds\n", i, time);
-		time = benchmark_sort(bogo_sort, i, 100000);
-		printf("bogo sorted %d entries in %f seconds\n", i, time);
+	sort_fn_info sort_fns[] = { { "bubble", bubble_sort },
+		{ "naieve bubble", naive_bubble_sort },
+		{ "insertion", insertion_sort} };
+	int sort_fn_count = 3;
+
+	int sort_upto = 10;
+
+	printf("\t");
+	for (int i = 0; i < sort_fn_count; i++) {
+		printf("%s\t", sort_fns[i].name);
+	}
+	printf("\n");
+	for (int i = 0; i < sort_upto; i++) {
+		printf("%d\t", i);
+		for (int j = 0; j < 3; j++) {
+			double time = benchmark_sort(sort_fns[j].sort_fn, i, 100000);
+			printf("%f\t", time);
+		}
 		printf("\n");
 	}
 }
