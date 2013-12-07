40f61d93bbf87e1c20af9b535ff566f5680abaa4
Julian Simioni <julian.simioni@gmail.com>
2013-12-07 12:02:33 -0800

    Use configurable step size for sort sizes

--- sort.c
+++ sort.c
@@ -51,14 +51,15 @@ int main()
 		{ "insertion", insertion_sort} };
 	int sort_fn_count = 3;
 
-	int sort_upto = 10;
+	int sort_upto = 1000;
+	int step = 100;
 
 	printf("\t");
 	for (int i = 0; i < sort_fn_count; i++) {
 		printf("%s\t", sort_fns[i].name);
 	}
 	printf("\n");
-	for (int i = 0; i < sort_upto; i++) {
+	for (int i = 0; i < sort_upto; i+=step) {
 		printf("%d\t", i);
 		for (int j = 0; j < 3; j++) {
 			double time = benchmark_sort(sort_fns[j].sort_fn, i, 100000);
