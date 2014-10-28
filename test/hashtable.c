24d7d03a2e884217b6c8dff592ae8724294171f2
Julian Simioni <julian.simioni@gmail.com>
2014-10-28 15:35:58 -0700

    Separate and expand hashtable tests

--- test/hashtable.c
+++ test/hashtable.c
@@ -5,28 +5,40 @@
 
 #include "hashtable.h"
 
-int main()
+void test_hash_function_same()
+{
+	//test hash function returns the same value for identical
+	//strings at different addresses
+	char* one = "test";
+	char* two = "test";
+
+	assert(&one != &two);
+	assert(default_hash(&one, 20) == default_hash(&two, 20));
+	assert(default_hash(one, 20) == default_hash("test", 20));
+}
+
+void test_hash_function_different()
+{
+	char* one = "one";
+	char* two = "two";
+
+	assert(default_hash(&one, 20) != default_hash(&two, 20));
+}
+
+void test_hash_function_long_compare()
+{
+	//test hash function looks at more than the first 4 bytes
+	char* one = "test";
+	char* two = "testlong";
+	assert(default_hash(one, 20) != default_hash(two, 20));
+}
+
+void test_hashtable_insert_and_find_simple()
 {
 	hashtable *ht = hashtable_init(20, default_hash);
 
 	hashtable_insert(ht, "foo", "bar");
 	hashtable_insert(ht, "hello", "world");
-
-	char** strings = malloc(sizeof(char*)*40);
-	int print_result;
-
-	for (int i = 0; i < 40; i++) {
-		strings[i] = malloc(sizeof(char[10]));
-		print_result = asprintf(&strings[i], "test%d", i);
-		if (print_result == -1) {
-			return -1;
-		}
-		hashtable_insert(ht, strings[i], strings[i]);
-	}
-
-	for (int i = 0; i < 40; i++) {
-		assert(hashtable_find(ht, strings[i]) == strings[i]);
-	}
 
 	assert(hashtable_find(ht, "hello") == "world");
 	assert(hashtable_find(ht, "foo") == "bar");
@@ -34,40 +46,52 @@ int main()
 	hashtable_insert(ht, "hello", "world2");
 	hashtable_find(ht, "hello");
 	assert(strcmp(hashtable_find(ht, "hello"), "world2") == 0);
+}
 
-	char *one, *two, *data;
-	print_result = asprintf(&one, "test");
-	if (print_result == -1) {
-			return -1;
-	}
-	print_result = asprintf(&two, "test");
-	if (print_result == -1) {
-			return -1;
-	}
-	print_result = asprintf(&data, "data");
-	if (print_result == -1) {
-			return -1;
-	}
-	assert(one != two);
+void test_hashtable_insert_and_find_many()
+{
+	hashtable *ht = hashtable_init(20, default_hash);
 
-	//test hash function returns the same value for identical
-	//strings at different addresses
-	assert(default_hash(one, 20) == default_hash(two, 20));
-	assert(default_hash(one, 20) == default_hash("test", 20));
+	char** strings = malloc(sizeof(char*)*40);
+	int print_result;
 
-	//test hash function looks at more than the first 4 bytes
-	char *three;
-	print_result = asprintf(&three, "testb");
-	if (print_result == -1) {
-			return -1;
+	for (int i = 0; i < 40; i++) {
+		strings[i] = malloc(sizeof(char[10]));
+		print_result = asprintf(&strings[i], "test%d", i);
+		assert(print_result != -1);
+		hashtable_insert(ht, strings[i], strings[i]);
 	}
-	assert(default_hash(one, 20) != default_hash(three, 20));
 
-	//test inserting and retrieving with indentical strings
+	for (int i = 0; i < 40; i++) {
+		assert(hashtable_find(ht, strings[i]) == strings[i]);
+	}
+}
+
+void test_hashtable_insert_find_identical()
+{
+	//test inserting and retrieving with identical strings
 	//at different addresses works
+	//
+	char* one = "test";
+	char* two = "test";
+	hashtable *ht = hashtable_init(20, default_hash);
+
+	char* data = "somedata";
 	hashtable_insert(ht, one, data);
 	char *out = hashtable_find(ht, two);
+
 	assert(0 != out);
 	assert(out == data);
 	assert(0 == strcmp(out, data));
+}
+
+int main()
+{
+	test_hash_function_same();
+	test_hash_function_different();
+	test_hash_function_long_compare();
+
+	test_hashtable_insert_and_find_simple();
+	test_hashtable_insert_and_find_many();
+	test_hashtable_insert_find_identical();
 }
