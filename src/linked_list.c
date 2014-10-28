11dcb0cf49f0c7d2dc55b140068435bd74f00ed9
Julian Simioni <julian.simioni@gmail.com>
2014-10-28 14:53:12 -0700

    Move linked list assertions to test program

--- src/linked_list.c
+++ src/linked_list.c
@@ -29,26 +29,4 @@ int main()
 	printf("\nRemoving the last element\n");
 	delete_tail_singly_linked_list(list);
 	print_singly_linked_list_node(list->head);
-
-	singly_linked_list *l1 = init_singly_linked_list();
-	singly_linked_list *l2 = init_singly_linked_list();
-
-	insert_singly_linked_list(l1, &data[0]);
-	insert_singly_linked_list(l1, &data[1]);
-	insert_singly_linked_list(l2, &data[2]);
-	insert_singly_linked_list(l2, &data[3]);
-
-	singly_linked_list *l3 = singly_linked_list_join(l1, l2);
-	singly_linked_list_node *node = l3->head;
-
-	int i;
-	for (i = 0; node != 0; i++) {
-		//check correct values are contained
-		assert(*(int*)(node->data) == data[i]);
-		node = node->next;
-	}
-	assert(i == 4);//check size
-
-	//ensure nodes in original lists are not modified
-	assert(l1->tail->next == 0);
 }
