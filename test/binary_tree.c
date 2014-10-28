c70b609695ff085329540efbafa802aa6ad93e44
Julian Simioni <julian.simioni@gmail.com>
2014-10-28 15:50:26 -0700

    Rename binary tree functions
    Use binary_tree instead of just tree, to allow for other types of trees.
    Use binary_tree as prefix instead of suffix.

--- test/binary_tree.c
+++ test/binary_tree.c
@@ -5,15 +5,15 @@ int main()
 	//some nice test data, randomized so it works ok with a non-self-balancing tree
 	int data[] = { 5, 9, 7, 2, 4, 8, 3, 1, 0, 6 };
 
-	tree_node *root = init_tree_node(data[0]);
+	binary_tree_node *root = binary_tree_node_init(data[0]);
 
 	//insert the data
 	for (int i = 1; i < 10; i++) {
-		tree_node *new = init_tree_node(data[i]);
-		insert_tree_node(root, new);
+		binary_tree_node *new = binary_tree_node_init(data[i]);
+		binary_tree_insert(root, new);
 	}
 
-	if(!test_tree_correctness(root)) {
+	if(!binary_tree_test_correctness(root)) {
 		return 1;
 	}
 }
